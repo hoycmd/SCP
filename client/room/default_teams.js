6931182a28954dabf0f2a63b5b0743ff21ec568a
BOSS-PIXELCOMBATS2 <144242217+BOSS-PIXELCOMBATS2@users.noreply.github.com>
2025-08-15 14:53:30 +0200

    Update default_teams.js

--- client/room/default_teams.js
+++ client/room/default_teams.js
@@ -15,10 +15,12 @@ export const blue_team_color = new basic.Color(1, 0.5, 0, 0);
 export function create_blue_team() {
  room.Teams.Add(blue_team_name, blue_team_display_name, blue_team_color);
  room.Teams.Get(blue_team_name).Spawns.SpawnPointsGroups.Add(blue_team_spawn_points_groups);
+ return room.Teams.Get(blue_team_name);
 }
 
 // команда красная
 export function create_red_team() {
  room.Teams.Add(red_team_name, red_team_display_name, red_team_color);
  room.Teams.Get(red_team_name).Spawns.SpawnPointsGroups.Add(red_team_spawn_points_groups);
+ return room.Teams.Get(red_team_name);
 }
